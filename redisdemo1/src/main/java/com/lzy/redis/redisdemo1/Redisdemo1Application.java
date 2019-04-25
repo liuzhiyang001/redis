de8a3d6a3e844a9b2cc4d8fc03fd276730c6a4db
liuzhiyang001 <2935268135@qq.com>
2019-04-25 15:52:16 +0800

    first commit

---
+++ redisdemo1/src/main/java/com/lzy/redis/redisdemo1/Redisdemo1Application.java
@@ -0,0 +1,13 @@
+package com.lzy.redis.redisdemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Redisdemo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Redisdemo1Application.class, args);
+    }
+
+}
---
+++ redisdemo1/src/test/java/com/lzy/redis/redisdemo1/Redisdemo1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lzy.redis.redisdemo1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Redisdemo1ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
